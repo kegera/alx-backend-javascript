d355e75653ab162ec877646a51c43d5be79c0d20
Caleb <kegeshaddy@gmail.com>
2024-05-14 23:21:53 +0300

    express http server

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,29 +1,55 @@
 const express = require('express');
-const students = require('./3-read_file_async');
+const fs = require('fs');
+const csv = require('csv-parser');
+
 const app = express();
-const hostname = '127.0.0.1';
 const port = 1245;
 
+// Define the route for the root path '/'
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
+    res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
-    });
+// Define the route for '/students'
+app.get('/students', (req, res) => {
+    let studentsData = '';
+    let studentsCount = 0;
+    let csStudentsCount = 0;
+    let sweStudentsCount = 0;
+    let csStudents = [];
+    let sweStudents = [];
+
+    fs.createReadStream(process.argv[2])
+        .pipe(csv())
+        .on('data', (row) => {
+            if (row.Name && row.Age && row.Field) {
+                studentsCount++;
+                if (row.Field === 'CS') {
+                    csStudentsCount++;
+                    csStudents.push(row.Name);
+                } else if (row.Field === 'SWE') {
+                    sweStudentsCount++;
+                    sweStudents.push(row.Name);
+                }
+            }
+        })
+        .on('end', () => {
+            studentsData += 'This is the list of our students
+';
+            studentsData += `Number of students: ${studentsCount}
+`;
+            studentsData += `Number of students in CS: ${csStudentsCount}. List: ${csStudents.join(', ')}
+`;
+            studentsData += `Number of students in SWE: ${sweStudentsCount}. List: ${sweStudents.join(', ')}
+`;
+
+            res.send(studentsData);
+        });
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;
